363881b1b3ca7643bb9713649e1f99b7b863ee6a
SOFIA TAMARA <46500882@ort>
2022-04-28 09:43:22 -0300

    TP1

--- dai/js/01-express.js
+++ dai/js/01-express.js
@@ -1,72 +1,170 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-const process_data = () => {
-
-    const data = [11, 9, 2, -1, 110, 99, 8];
-
-    let impar = [];
-    
-    for (let i = 0; i < data.length; i++){
-    
-        const element = data[1];
-    
-        if(element%2 !==0){
-    
-            impar.push(element);
-        }
-    }
-    
-    const proc = impar.sort((a,b)=>a-b);
-
-    /* el .sort ordena alfabéticamente*/
-    console.log(proc[1]);
-
-    return {
-
-        resultado : proc[1]
-    };
-}
-
-    /*let Numeros = [11,9,2,-1,110,99,8];
-    let par = [];
-    let impar = [];
-
-
-    function MostrarNumeroPar(){
-
-        Numeros.forEach(num => {
-
-            if (num%2 == 0) par.push(num);
-            else {
-                impar.push(num)
-            }
-        })
-
-        res.send(num);
-
-        Numeros.sort((a,b)=>a-b)
-        res.send(Numeros[1]);
-    } 
-};
-
-*/
-
-
-
+app.use(express.json());
+/*
+const min = 1;
+const numerosCarton = 10;
+let cartones = [];
+let numerosSacados = [];
+let ganadores = [];
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
+function NumeroAleatorio(max){
+    return Math.floor(Math.random() * (max - 1)) + 1;
+}
 
-    console.log(req.body);
-    res.send(process_data(req.body));
 
-});
+function crearCartones(num){
+    let funciono = -1, numAleatorio, a, cantidad;
+    for (let i = 0; i < num; i++) {
+        let carton = {
+            id: i,
+            nombre: null,
+            valores: [],
+        }
+        a = 0
+        while(a < numerosCarton) {
+            numAleatorio = NumeroAleatorio()
+            b = 0
+            while(b < i){ // Este while verifica que el numero aleatorio no este ya en el carton
+                if(cartones[i].valores[b] == numAleatorio){
+                    b = i + 1
+                }
+                b++
+            }
+            if(b != i+2){ // si el numero aleatorio no esta en el carton, lo agrega, en caso contrario no hace nada
+                carton.valores.push(numAleatorio)
+                a++
+            }
+        }
+        cartones.push(carton)
+    }
+    cantidad = cartones[NumeroAleatorio(num)].valores.length // linea para verificar que la funcion funcione correctamente
+    if(cantidad != numerosCarton){
+        funciono = 1 // ERROR CODE 1
+    }
+    return funciono;
+}
 
-app.listen(port, () => {
+function iniciarJuego(num){
+    if(max == -1){
+        max = 99
+    }
+    let funciono = crearCartones(num);
+    return funciono;
+}
+    function obtenerCarton(nombre){
+        let faltaEncontrar = true, cartonRandom;
+        while(faltaEncontrar){ //busca un carton al azar el cual no pertenezca a ninguna persona
+            cartonRandom = NumeroAleatorio(carton.length);
+            if(carton[cartonRandom].nombre == null){
+                faltaEncontrar = false;
+            }
+        }
+        cartones[cartonRandom].nombre = nombre
+        return cartones[cartonRandom].valores;
+    }
+*/
+    function NumeroAleatorio(max){
+        return Math.floor(Math.random() * (max - 1)) + 1;
+    }
 
-    console.log (`Example app listening on port ${port}`)
+    let cartones = []
 
-});
+    function crearCarton(){
+        let carton = {
+            nombre: null,
+            valores: [],      
+        }
+        for (let index = 0; index < 10; index++) {
+            carton.valores.push(NumeroAleatorio(10))
+        
+        }
+        return carton;
+    }
+
+    function crearCartones(num){
+
+        for (let index = 0; index < num; index++) {
+            cartones.push(crearCarton(10))
+        
+        }
+        return cartones;
+    }
+
+
+    function iniciarJuego(num){
+        return crearCartones(num);
+    }
+
+    function obtenerCarton(nombre){
+        for (let index = 0; index < cartones.length; index++) {
+            cartones[index].nombre = nombre;
+        }
+        while(nombre.carton = "" ){
+            carton == nombre;
+        }
+        return cartones;
+    }
+
+    function devolverCartones(nombre){
+        if(nombre == ""){
+            return cartones;
+        }
+        else{
+            return obtenerCarton(nombre);
+        }
+    }
+    let numerosSacados = [];
+
+    function sacarNumero(num){
+        let numeroRandom = NumeroAleatorio(10);
+        numerosSacados.push(numeroRandom);
+        cartones.forEach(c => {
+            for (let i = 0; i > c.valores; i++) {
+                if (c.valores[i] = numeroRandom){
+                    c.valores[i].pop;
+                }
+            } 
+        }}
+
+    app.post('/numero_aleatorio', (req, res)=>{
+    
+        console.log(req.body.numero);
+        let x = NumeroAleatorio(req.body.numero);
+        res.send([x]);
+        
+        })
+        
+        
+        
+    app.post('/iniciar_Juego', (req, res)=>{
+        
+        console.log(req.body.cartones);    
+        let y = iniciarJuego(req.body.cartones);    
+        res.send([y]);
+        
+        
+    })
+
+    app.get('/obtener_Carton', (req, res)=>{
+   
+        console.log(req.body.nombre);    
+         let s = obtenerCarton(req.body.nombre);    
+         res.send(s);
+       
+     
+    })
+
+    app.get('/cartones', (req, res)=>{
+        console.log(req.body.nombre);
+        let cartonesAMostrar = devolverCartones(req.body.nombre);
+        res.send(cartonesAMostrar);
+    })
+
+
+    app.listen(port, () => {
+        console.log(`example app listening on port ${port}`);
+    });
