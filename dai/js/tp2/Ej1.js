06b4fde3937fba23dda090cd3f5bc79a6a546c68
Jade <jade.arevort@gmail.com>
2022-05-26 17:07:31 -0300

    Dai

---
+++ dai/js/tp2/Ej1.js
@@ -0,0 +1,9 @@
+const string1 = "Jade";
+const string2 = "Arevalos";
+
+
+//Interpolación
+console.log(`Hola! ${string1} ${string2}`);
+
+//Concatenación
+console.log(string1 + string2);
---
+++ dai/js/tp2/modules/matematica.js
@@ -0,0 +1,25 @@
+//4 FUNCIONES
+
+let x = 20;
+let y = 15;
+
+const Sumar = (x,y) => {
+
+    console.log(x+y);
+}
+
+const Restar = (x,y) => {
+
+console.log(x-y);
+
+}
+
+const Multiplicar = (x,y) => {
+
+    console.log(x*y);
+}
+
+const Dividir = (x,y) => {
+
+    console.log(x/y);
+}
